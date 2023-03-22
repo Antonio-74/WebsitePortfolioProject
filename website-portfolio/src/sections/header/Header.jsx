a0b02fca4cc386b830b78c6d95f0ca47df62cd9d
Antonio-74 <antoniolopezvalles604@gmail.com>
2023-03-21 22:26:09 -0700

    Navbar and Header section created

--- website-portfolio/src/sections/header/Header.jsx
+++ website-portfolio/src/sections/header/Header.jsx
@@ -1,9 +1,46 @@
 import React from 'react';
+import HeaderImage from '../../assets/header.jpg';
+import { AiOutlineInstagram } from 'react-icons/ai';
+import { AiOutlineTwitter } from 'react-icons/ai';
+import { AiOutlineDribbble} from 'react-icons/ai';
+import { AiFillGithub } from 'react-icons/ai';
 import './header.css';
 
 function Header() {
+
+  const data = [
+    { id: 1, link: 'https"//instagram.com', icon: <AiOutlineInstagram /> },
+    { id: 2, link: 'https"//twitter.com', icon: <AiOutlineTwitter /> },
+    { id: 3, link: 'https"//dribbble.com', icon: <AiOutlineDribbble /> },
+    { id: 4, link: 'https"//github.com', icon: <AiFillGithub /> },
+  ];
+
   return (
-    <header id='header'><h2>Header</h2></header>
+    <header id='header'>
+      <div className="container header__container">
+        <div className="header__profile">
+          <img src={HeaderImage}  alt='Header Portait' />
+        </div>
+        <h3>Aera White</h3>
+        <p>
+          You are a click away from building your dream website or web app. Send me the details of your project for a modern, mobile responsive, highly performant website today!
+        </p>
+        <div className="header__cta">
+          <a href='#contact' className='btn primary'>Let's Talk</a>
+          <a href='#portfolio' className='btn light'>My Work</a>
+        </div>
+        <div className='header__socials'>
+          {data.map(item => 
+            <a 
+              key={item.id} 
+              href={item.link}
+              target='_blank'
+              rel='noopener noreferrer'
+            >{item.icon}</a>
+          )}
+        </div>
+      </div>
+    </header>
   )
 }
 
--- website-portfolio/src/sections/navbar/Navbar.jsx
+++ website-portfolio/src/sections/navbar/Navbar.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import Logo from '../../assets/logo.jpg';
+import { IoIosColorPalette } from 'react-icons/io';
 import './navbar.css';
 
 function Navbar() {
+  
+  const data = [
+    { id: 1, link: '#', title: 'Home' },
+    { id: 2, link: '#about', title: 'About' },
+    { id: 3, link: '#services', title: 'Services' },
+    { id: 4, link: '#portfolio', title: 'Portfolio' },
+    { id: 5, link: '#contact', title: 'Contact' }
+  ];
+
   return (
-    <nav><h2>Navbar</h2></nav>
+    <nav>
+      <div className="container nav__container">
+        <a href='index.html'>
+          <img src={Logo} className='nav__logo' alt='Logo' />
+        </a>
+        <ul className='nav__menu'>
+          {data.map(item => <li key={item.id}><a href={item.link}>{item.title}</a></li>)}
+        </ul>
+        <button id='theme__icon'>
+          <IoIosColorPalette />
+        </button>
+      </div>
+    </nav>
   )
 }
 
