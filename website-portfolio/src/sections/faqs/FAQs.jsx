4730fd721ce4cd16afd4e413af311fb1e80cde4f
Antonio-74 <antoniolopezvalles604@gmail.com>
2023-03-30 17:24:55 -0700

    FAQ section created

--- website-portfolio/src/sections/faqs/FAQs.jsx
+++ website-portfolio/src/sections/faqs/FAQs.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import faqs from './data';
+import FAQ from './FAQ';
 import './faqs.css';
 
 function FAQs() {
   return (
-    <section id='faqs'><h2>FAQs</h2></section>
+    <section id='faqs'>
+      <h2>Frequenly Asked Questions</h2>
+      <p>
+        Here are some questions I usually get. Click to toggle the answer, and if you still
+        heve some moe questions, shoot me a message from the contact section!
+      </p>
+      <div className="container faqs__container">
+        {faqs.map(faq => (
+          <FAQ key={faq.id} faq={faq} />
+        ))}
+      </div>
+    </section>
   )
 }
 
